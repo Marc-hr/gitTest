22b36c5491491c1fdd37e24f34c96f23194a4cb2
Marc-hr <1365684814@qq.com>
2019-09-29 14:15:57 +0800

    git测试

---
+++ src/main/java/cn/yunhe/servlet/MavenServlet.java
@@ -0,0 +1,25 @@
+package cn.yunhe.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MavenServlet
+ */
+public class MavenServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public MavenServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().write("我和我的祖国");
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/test/java/cn/yunhe/test/MavenTest.java
@@ -0,0 +1,10 @@
+package cn.yunhe.test;
+
+import org.junit.Test;
+
+public class MavenTest {
+	@Test
+	public void test() {
+		System.out.println("我爱我的祖国");
+	}
+}
